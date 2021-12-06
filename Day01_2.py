be856acb8985d3e15ac3144e8d10b6d587574c69
Amit Arya <amitarya192@gmail.com>
2021-12-06 19:56:21 +0530

    Update Day01_2.py

--- Day01_2.py
+++ Day01_2.py
@@ -1,24 +1,18 @@
 # Main Method area.
 if __name__ == "__main__":
-    c = 0
+    count = 0
     lis = []
-    fin = open(r"C:\Users\ACER\OneDrive\Documents\Python-Workspace\BASICS\day1_input.txt","r")
-    print("Input: ")
+    fin = open(r"day1.txt","r")
     for i in fin:
-        print(type(i))
         lis.append(int(i))
         
     l = len(lis)
-    print("size of input",l)
     output = []
     for i in range(0,l-2):
         sum = 0
-        # 1 2 3 4
-        # 0 1 2 3
         sum = lis[i] + lis[i+1] + lis[i+2]
-        print("Sum at &i",i,sum)
         output.append(sum)
     for i in range(1,len(output)):
         if output[i] > output[i-1]:
-            c = c + 1
-    print(c) 
+            count = count + 1
+    print(count) 
