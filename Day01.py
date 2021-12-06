8f690491362c188b78d54ddc646fa7eabb868341
Amit Arya <amitarya192@gmail.com>
2021-12-06 19:53:58 +0530

    Update Day01.py

--- Day01.py
+++ Day01.py
@@ -1,19 +1,17 @@
 # Main Method area.
 if __name__ == "__main__":
-    c = 0
+    count = 0
     lis = []
-    fin = open(r"C:\Users\ACER\OneDrive\Documents\Python-Workspace\BASICS\day1_input.txt","r")
+    fin = open(r"day01.txt","r")
     for i in fin:
         lis.append(i)
-    print(len(lis))
-    for i in range(len(lis)):
-        for j in range(i+1,len(lis)):
-            if lis[j] > lis[i]:
-                c += 1
-                break
-            else:
-                break
-    print(c)
+    for i in range(1,len(lis)):
+        if lis[i] > lis[i-1]:
+            c += 1
+            break
+        else:
+            break
+    print(count)
     
     
 
