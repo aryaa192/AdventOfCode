992c24d32a4e56e343825f9908dba0c03cfa0074
Amit Arya <amitarya192@gmail.com>
2023-01-12 06:15:40 +0000

    update part

--- 2022/Day03/main.py
+++ 2022/Day03/main.py
@@ -2,12 +2,39 @@
 # Date: 
 
 # Problem Descriptions
+def get_sliced(items):
+    c1, c2 = [], []
+    c1, c2 = items[:len(items)//2], items[len(items)//2:]
+
+    return c1, c2
+
+def get_priorityItems(rucksucks):
+    priorities = []
+    for items in rucksucks:
+        c1, c2 = get_sliced(items)
+        for i in c1:
+            if i in c2 and not in priorities:
+                priorities.append(i)
+    
+    return priorities
+
+def parse_input():
+    with open(r's_input.txt', 'r') as f:
+        data = f.readlines()
+        rucksucks = [data_s.strip() for data_s in data]
+    
+    f.close()
+    return rucksucks
 
 
 # main section
 if __name__ == '__main__':
-    with open(r's_input.txt', 'r') as f:
-        data = f.readlines()
-        ruck_sucks_compartments = [ data_s.strip() for data_s in data]
 
-        
+    input = parse_input()
+    getPriorityItems = get_priorityItems(input)
+    v = 0
+    priorities
+    for i = 'a'; i<='z'
+
+    
+
