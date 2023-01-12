74abff3f7be5d9fb8e0af60ae35279c81427364c
Amit Arya <amitarya192@gmail.com>
2023-01-12 05:28:22 +0000

    Day03 started

---
+++ 2022/Day03/main.py
@@ -0,0 +1,13 @@
+# Author: Amit Arya 
+# Date: 
+
+# Problem Descriptions
+
+
+# main section
+if __name__ == '__main__':
+    with open(r's_input.txt', 'r') as f:
+        data = f.readlines()
+        ruck_sucks_compartments = [ data_s.strip() for data_s in data]
+
+        
