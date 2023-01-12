53947a0fa819336dbdaf137848f6fca824c3fe66
Amit Arya <amitarya192@gmail.com>
2023-01-12 07:35:28 +0000

    update on day03

--- 2022/Day03/main.py
+++ 2022/Day03/main.py
@@ -1,6 +1,10 @@
 # Author: Amit Arya 
 # Date: 
 
+# Import Section
+import string
+
+
 # Problem Descriptions
 def get_sliced(items):
     c1, c2 = [], []
@@ -8,18 +12,36 @@ def get_sliced(items):
 
     return c1, c2
 
-def get_priorityItems(rucksucks):
+
+def get_priority_items(rucksucks):
     priorities = []
     for items in rucksucks:
         c1, c2 = get_sliced(items)
         for i in c1:
-            if i in c2 and not in priorities:
+            if i in c2 and i not in priorities:
                 priorities.append(i)
-    
     return priorities
 
+
+def get_priorities_sum(priorities_items):
+    v = 1
+    priorities_value = {}
+    lowers = list(string.ascii_lowercase)
+    uppers = list(string.ascii_uppercase)
+    for i in lowers:
+        priorities_value[i] = v
+        v += 1
+    for i in uppers:
+        priorities_value[i] = v
+        v += 1
+    sum = 0
+    for item in priorities_items:
+        sum += priorities_value[item]
+    return sum
+
+
 def parse_input():
-    with open(r's_input.txt', 'r') as f:
+    with open(r'input.txt', 'r') as f:
         data = f.readlines()
         rucksucks = [data_s.strip() for data_s in data]
     
@@ -31,10 +53,11 @@ def parse_input():
 if __name__ == '__main__':
 
     input = parse_input()
-    getPriorityItems = get_priorityItems(input)
-    v = 0
-    priorities
-    for i = 'a'; i<='z'
+    PriorityItems = get_priority_items(input)
+    prioritySum = get_priorities_sum(PriorityItems)
+    print(prioritySum)
+
+
 
     
 
